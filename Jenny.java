6f212bad8f141fd7d0ca7f1f01f8926926a37d54
gboreczek <103647431+gboreczek@users.noreply.github.com>
2022-04-14 15:47:21 +0200

    Update Jenny.java

--- Jenny.java
+++ Jenny.java
@@ -1,5 +1,5 @@
 public class Jenny{
 	public static void main (String[] args){
-		System.out.println("Jenny, oh Jenny, Joy, ooh Joy!");
+		System.out.println("Jenny, oh Jenny, Joy, ooh Joy! Misty, ooh Misty!");
 	}
 }
